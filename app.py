a43b79b5d075fb7b29bb17902ba6c666feaa1fee
concejomunipalchcao <serviciocomunitario.daoc2025@gmail.com>
2026-03-22 23:53:48 -0400

    agrega py 7

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -17,8 +18,6 @@ def envios():
 @app.route("/clientes")
 def clientes():
     return render_template("clientes.html")
-
-import os
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
